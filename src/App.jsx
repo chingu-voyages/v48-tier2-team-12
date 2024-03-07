d29b0a319847680b1419bb18ff4f145b91dcafac
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-03-07 15:56:52 +0100

    placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <img src="./src/assets/chingu-logo.png" height="50px"></img>
+    <div className="main">
+      <img className="logo" src="./src/assets/chingu-logo.png" height="50px"></img>
       <h1>Voyage 48 - Team 12</h1>
     </div>
   )
