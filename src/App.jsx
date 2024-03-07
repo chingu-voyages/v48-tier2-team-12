4a9fd08f83941c255d48447c604237bc7ccfd6e9
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-03-07 15:44:56 +0100

    logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <img src="./src/assets/chingu-logo"></img>
+      <img src="./src/assets/chingu-logo.png"></img>
       <h1> - Voyage 48 - Team 12</h1>
     </div>
   )
