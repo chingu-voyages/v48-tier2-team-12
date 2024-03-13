ffe853073e26f67be15fa762f90c2c89dd130d2c
Morais-C <morais.carlos@hotmail.com>
2024-03-13 18:49:58 +0000

    add import from /interfaces/dino.interface

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,5 @@
+import { Dino } from "../interfaces/dino.interface";
+
 export async function fetchDinos(): Promise<Dino[]> {
     const response = await fetch("https://chinguapi.onrender.com/dinosaurs");
     const data = await response.json();
