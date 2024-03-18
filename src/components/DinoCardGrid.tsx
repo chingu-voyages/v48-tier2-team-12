23226eea62315fbc39f1c96d6d5dff36804da769
Morais-C <morais.carlos@hotmail.com>
2024-03-18 14:51:44 +0000

    DinoCard Component and DinoCard Grid

--- src/components/DinoCardGrid.tsx
+++ src/components/DinoCardGrid.tsx
@@ -1,9 +1,15 @@
+import { DinoCard } from "./DinoCard";
 import classes from "./DinoCardGrid.module.css";
 
 export function DinoCardGrid() {
   return (
     <div className={classes.smallCardGrid}>
-      <div></div>
+      <div className={classes.smallCardGridRow}>
+        <DinoCard/>
+        <DinoCard/>
+        <DinoCard/>
+
+      </div>
     </div>
   );
 }
