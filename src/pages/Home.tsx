f24bb99d524e8f66d9f0ae2d0caa7bc6ebe8fd79
bulascoskun <ulas_coskun99@hotmail.com>
2024-03-18 21:32:28 +0300

    changed-image-importing-method

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
-import ShowDino from "../components/Alternative-ShowDinos";
-import CategoryTiles from "../components/CategoryTiles";
-import { DinoCardGrid } from "../components/DinoCardGrid";
+import ShowDino from '../components/Alternative-ShowDinos';
+import CategoryTiles from '../components/CategoryTiles';
+import { DinoCardGrid } from '../components/DinoCardGrid';
+
+import filterButton from '../assets/filter.png';
 
 export default function Home() {
   return (
@@ -9,7 +11,7 @@ export default function Home() {
       <div className="search-div">
         <input className="search-bar" placeholder="T-Rex" />
         <button className="filter-btn">
-          <img alt="Filter Button" src="src\assets\filter.png" />
+          <img alt="Filter Button" src={filterButton} />
         </button>
       </div>
       <CategoryTiles />
