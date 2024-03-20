0f24ea2760a69e5bba1912789fb529463f0342e7
Morais-C <morais.carlos@live.com>
2024-03-20 13:00:52 +0000

    Rendering Dynamic DinoCards Grid from API - WIP

--- src/components/DinoCard.tsx
+++ src/components/DinoCard.tsx
@@ -1,14 +1,15 @@
+import { Dino } from "../interfaces/dino.interface";
 import classes from "./DinoCard.module.css";
 
-export function DinoCard() {
+export function DinoCard(props: Dino) {
   return (
     <div className={classes.smallCard}>
       <img
         className={classes.smallCardImage}
-        src="https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2022/06/fausto-garcia-menendez-hYKG311mff8-unsplash.jpg.png"
+        src={props.imageSrc}
         alt=""
       />
-        <div className={classes.smallCardTitle}>Title</div>
+        <div className={classes.smallCardTitle}>{props.name}</div>
       
     </div>
   );
--- src/components/DinoCardGrid.tsx
+++ src/components/DinoCardGrid.tsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import { DinoCard } from "./DinoCard";
+
+import { fetchDinos } from "../utils/api";
+import { Dino } from "../interfaces/dino.interface";
 import classes from "./DinoCardGrid.module.css";
 
 export function DinoCardGrid() {
-  return (
+  const [dinos, setDinos] = useState<Dino[]>([]);
+  useEffect(() => {
+    fetchDinos().then((data) => setDinos(data));
+  }, []);
+
+  return  (
     <>
-      <h2 className="smallCardGridTitle">Discover</h2>
+      {/* <h2 className={classes.smallCardGridTitle}>Discover</h2> */}
       <div className={classes.smallCardGrid}>
-        <DinoCard />
-        <DinoCard />
-        <DinoCard />
-        <DinoCard />
+        
+      {dinos.map((dino) => (
+        
+        <DinoCard
+          imageSrc= {dino.imageSrc} //"https://stories.cnnbrasil.com.br/wp-content/uploads/sites/9/2022/06/fausto-garcia-menendez-hYKG311mff8-unsplash.jpg.png"
+          
+          name= {dino.name} // "Dino Name"
+          type=""
+          description=""
+          foundIn=""
+        />
+        ))}
       </div>
     </>
   );
