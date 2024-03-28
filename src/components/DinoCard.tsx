9a00f7b716a8646e9522c0bafb5c371e1bfe9723
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-03-28 18:18:10 +0100

    refactoring

--- src/components/DinoCard.tsx
+++ src/components/DinoCard.tsx
@@ -4,9 +4,17 @@ import { Link } from 'react-router-dom';
 
 export function DinoCard(props: Dino) {
   return (
-    <Link to={`/dino/${props.id}`} className={styles.smallCard}>
-      <img className={styles.smallCardImage} src={props.imageSrc} alt={`image of ${props.name}`} />
-      <div className={styles.smallCardTitle}>{props.name}</div>
+    <Link 
+      to={`/dino/${props.id}`} 
+      className={styles.smallCard}>
+        <img 
+          className={styles.smallCardImage} 
+          src={props.imageSrc} 
+          alt={`image of ${props.name}`} 
+        />
+        <div className={styles.smallCardTitle}
+        >{props.name}
+        </div>
     </Link>
   );
 }
--- src/components/DinosaurOfTheDay.tsx
+++ src/components/DinosaurOfTheDay.tsx
@@ -5,22 +5,28 @@ import styles from '../css-modules/DinosaurOfTheDay.module.css'
 import { Link } from "react-router-dom";
 import { altPics } from "../utils/pretty-pics.tsx";
 
-function DinosaurOfTheDay() {
+export default function DinosaurOfTheDay() {
+
     const [dinoOfTheDay, setDinoOfTheDay] = useState<Dino>(Object);
     const [isExpanded, setIsExpanded] = useState(false);
     const [isLearnMoreVisible, setIsLearnMoreVisible] = useState(false);
+
     useEffect(() => {
         const fetchDinoOfTheDay = async () => {
             const allDinos = await fetchDinos();
+            // Mapping and replacing images
             allDinos.map(item => {
                 altPics.forEach(element => {
                   item.id === element.id ? item.imageSrc = element.img : ""
                 });
-                
-              }) 
+            })
+
             let dinoOfTheDay: Dino|null = null;
             while (!dinoOfTheDay) {
+
                 const randomIndex = Math.floor(Math.random() * allDinos.length)
+
+                // Checking for dinos with image and description
                 if (allDinos[randomIndex].imageSrc !== 'N/A' && 
                 allDinos[randomIndex].description !== 'N/A') {
                     dinoOfTheDay = allDinos[randomIndex]
@@ -36,23 +42,42 @@ function DinosaurOfTheDay() {
 
     const toggleExpand = () => setIsExpanded(prevExpanded => !prevExpanded)
 
-    return  (
+    return (
         <div className={styles['dino-of-the-day']}>
             <h1 className={styles['dino-of-the-day__heading']}>Dinosaur of the day</h1>
 
-            <Link to={`/dino/${dinoOfTheDay.id}`} className={styles['dino-of-the-day__card']}>
-                    {dinoOfTheDay.imageSrc !== 'N/A' && <img src={dinoOfTheDay.imageSrc} className={styles['dino-of-the-day__image']} alt={`an image of ${dinoOfTheDay.name}`}/>}
+            <Link 
+                to={`/dino/${dinoOfTheDay.id}`} 
+                className={styles['dino-of-the-day__card']}>
+
+                    {/* Image */}
+                    <img src={dinoOfTheDay.imageSrc} 
+                        className={styles['dino-of-the-day__image']} 
+                        alt={`an image of ${dinoOfTheDay.name}`}/>
+
+                    {/* Info */}
                     <div className={styles['dino-of-the-day__text']}>
-                        <h2 className={styles['dino-of-the-day__title']}>{dinoOfTheDay.name}</h2>
-                        <p className={`${styles['dino-of-the-day__description']} ${isExpanded ? styles['expanded'] : styles['collapsed']}`}
+                        <h2 className={styles['dino-of-the-day__title']}
+                        >{dinoOfTheDay.name}
+                        </h2>
+                        <p  className={`
+                            ${styles['dino-of-the-day__description']} 
+                            ${isExpanded ? 
+                            styles['expanded'] : 
+                            styles['collapsed']
+                            }`}
                         >
                             {dinoOfTheDay.description}
                         </p>
-                        {isLearnMoreVisible && <button className={styles['learn-more__btn']} onClick={toggleExpand}>{isExpanded ? 'Show Less' : 'Learn More'}</button>}
+                        {isLearnMoreVisible && <button 
+                            className={styles['learn-more__btn']} 
+                            onClick={toggleExpand}>
+                                {isExpanded ? 
+                                'Show Less' : 
+                                'Learn More'}
+                        </button>}
                     </div>
             </Link>
         </div>
     )
 }
-
-export default DinosaurOfTheDay;
