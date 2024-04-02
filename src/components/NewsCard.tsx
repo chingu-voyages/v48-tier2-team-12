d75b63ef8ac0f7c27d39741118ba3edd11879494
Huda Mabkhoot <hudamabkhoot@gmail.com>
2024-04-02 21:13:15 +0300

    Update NewsCard.tsx

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -1,11 +1,15 @@
 import classes from "../css-modules/NewsCard.module.css";
 import { Article } from "../interfaces/article.interface";
+import {Link} from "react-router-dom"
 
 export function NewsCard(article: Article) {
   return (
     <div className={classes.newsCard}>
       <div className={classes.newsCardHeadline}>{article.title}</div>
-      <div className={classes.newsCardSummary}>{article.description}</div>
+      <div className={classes.newsCardSummary}>
+        {article.description}
+        <Link to={{ pathname: article.url }} target="_blank" className={classes.newsCardSummaryLink}>Read More</Link>
+      </div>
     </div>
   );
 }
