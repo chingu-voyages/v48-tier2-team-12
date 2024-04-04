0fa03a12917f316adcebb262c8fcca249fd0ee16
Huda Mabkhoot <hudamabkhoot@gmail.com>
2024-04-04 05:55:30 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import DinoPage from './pages/DinoPage';
+import ChartsPage from './pages/ChartsPage';
 import SearchResults from './pages/SearchResults';
 import { FilterProvider } from './components/Filter/FilterContext';
 
@@ -28,6 +29,10 @@ const router = createBrowserRouter([
     path: '/dino/:id',
     element: <DinoPage />,
   },
+  {
+    path: '/charts',
+    element: <ChartsPage />,
+  }
 ]);
 
 const App = () => {
