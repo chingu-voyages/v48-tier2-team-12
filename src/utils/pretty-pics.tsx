c20625fb4fc44647b211ef0da214b1ea38c9979e
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-04-04 23:13:34 +0200

    more-pretty-imgs

--- src/utils/pretty-pics.tsx
+++ src/utils/pretty-pics.tsx
@@ -8,6 +8,27 @@ import styracosaurus from '../assets/haghani/styracosaurus.jpg';
 import ceratosaurus from '../assets/haghani/ceratosaurus.jpg';
 import aardonyx from '../assets/haghani/aardonyx.jpg';
 import aardonyxSmall from '../assets/haghani/aardonyx-small.jpg';
+import amargasaurus from '../assets/haghani/amargasaurus.jpg';
+import apatosaurus from '../assets/haghani/apatosaurus.jpg';
+import camarasaurus from '../assets/haghani/camarasaurus.jpg';
+import carcharodontosaurus from '../assets/haghani/carcharodontosaurus.jpg';
+import confuciusornis from '../assets/haghani/confuciusornis.jpg';
+import daspletosaurusVsEuoplocephalus from '../assets/haghani/DaspletosaurusVsEuoplocephalus.jpg';
+import deinocheirus from '../assets/haghani/deinocheirus.jpg';
+import einiosaurus from '../assets/haghani/einiosaurus.jpg';
+import euoplocephalus from '../assets/haghani/euoplocephalus.jpg';
+import gorgosaurus from '../assets/haghani/gorgosaurus.jpg';
+import lambeosaurus from '../assets/haghani/lambeosaurus.jpg';
+import leptoceratops from '../assets/haghani/leptoceratops.jpg';
+import mononykus from '../assets/haghani/mononykus.jpg';
+import nothronychus from '../assets/haghani/nothronychus.jpg';
+import ornithomimus from '../assets/haghani/ornithomimus.jpg';
+import sauropelta from '../assets/haghani/sauropelta.jpg';
+import scelidosaurus from '../assets/haghani/scelidosaurus.jpg';
+import spinosaurus from '../assets/haghani/spinosaurus.jpg';
+import stegosaurus from '../assets/haghani/stegosaurus.jpg';
+import triceratops from '../assets/haghani/triceratops.jpg';
+import utahraptor from '../assets/haghani/utahraptor.jpg';
 
 type Pretty = {
     title: string;
@@ -18,49 +39,149 @@ type Pretty = {
 
 export const altPics :Pretty[] = [
     {
-    title: 'albertosaurus',
-    id: 11,
-    img: albertosaurus,
+        title: 'albertosaurus',
+        id: 11,
+        img: albertosaurus,
     },
     {
-    title: 'pachycephalosaurus',
-    id: 208,
-    img: pachycephalosaurus,
+        title: 'pachycephalosaurus',
+        id: 208,
+        img: pachycephalosaurus,
     },
     {
-    title: 'tarbosaurus',
-    id: 282,
-    img: tarbosaurus,
+        title: 'tarbosaurus',
+        id: 282,
+        img: tarbosaurus,
     },
     {
-    title: 'tyrannosaurus',
-    id: 297,
-    img: tyrannosaurus,
+        title: 'tyrannosaurus',
+        id: 297,
+        img: tyrannosaurus,
     },
     {
-    title: 'velociraptor',
-    id: 304,
-    img: velociraptor,
+        title: 'velociraptor',
+        id: 304,
+        img: velociraptor,
     },
     {
-    title: 'parasaurolophus',
-    id: 213,
-    img: parasaurolophus,
+        title: 'parasaurolophus',
+        id: 213,
+        img: parasaurolophus,
     },
     {
-    title: 'ceratosaurus',
-    id: 59,
-    img: ceratosaurus,
+        title: 'ceratosaurus',
+        id: 59,
+        img: ceratosaurus,
     },
     {
-    title: 'styracosaurus',
-    id: 277,
-    img: styracosaurus,
+        title: 'styracosaurus',
+        id: 277,
+        img: styracosaurus,
     },
     {
-    title: 'aardonyx',
-    id: 1,
-    img: aardonyx,
-    smallImg: aardonyxSmall,
+        title: 'aardonyx',
+        id: 1,
+        img: aardonyx,
+        smallImg: aardonyxSmall,
+    },
+    {
+        title: 'amargasaurus',
+        id: 16,
+        img: amargasaurus,
+    },
+    {
+        title: 'apatosaurus',
+        id: 25,
+        img: apatosaurus,
+    },
+    {
+        title: 'camarasaurus',
+        id: 52,
+        img: camarasaurus,
+    },
+    {
+        title: 'carcharodontosaurus',
+        id: 54,
+        img: carcharodontosaurus,
+    },
+    {
+        title: 'confuciusornis',
+        id: 75,
+        img: confuciusornis,
+    },
+    {
+        title: 'deinocheirus',
+        id: 81,
+        img: deinocheirus,
+    },
+    {
+        title: 'einiosaurus',
+        id: 96,
+        img: einiosaurus,
+    },
+    {
+        title: 'euoplocephalus',
+        id: 106,
+        img: euoplocephalus,
+    },
+    {
+        title: 'gorgosaurus',
+        id: 121,
+        img: gorgosaurus,
+    },
+    {
+        title: 'lambeosaurus',
+        id: 153,
+        img: lambeosaurus,
+    },
+    {
+        title: 'leptoceratops',
+        id: 156,
+        img: leptoceratops,
+    },
+    {
+        title: 'mononykus',
+        id: 184,
+        img: mononykus,
+    },
+    {
+        title: 'nothronychus',
+        id: 197,
+        img: nothronychus,
+    },
+    {
+        title: 'ornithomimus',
+        id: 202,
+        img: ornithomimus,
+    },
+    {
+        title: 'sauropelta',
+        id: 249,
+        img: sauropelta,
+    },
+    {
+        title: 'scelidosaurus',
+        id: 252,
+        img: scelidosaurus,
+    },
+    {
+        title: 'spinosaurus',
+        id: 269,
+        img: spinosaurus,
+    },
+    {
+        title: 'stegosaurus',
+        id: 272,
+        img: stegosaurus,
+    },
+    {
+        title: 'triceratops',
+        id: 291,
+        img: triceratops,
+    },
+    {
+        title: 'utahraptor',
+        id: 302,
+        img: utahraptor,
     },
 ]
