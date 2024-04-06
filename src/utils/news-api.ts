f3619f9a463ca46e4f20e428bbb492075637aac4
dawooddilawar <d.dilawar94@gmail.com>
2024-04-06 15:56:17 +0200

    - fix news reload after 24 hours

--- src/utils/news-api.ts
+++ src/utils/news-api.ts
@@ -3,7 +3,7 @@ import { GNewsResponse } from "../interfaces/article.interface";
 export async function fetchNews(): Promise<GNewsResponse> {
   // Request a Key on https://gnews.io - limit with Free Account = 100 requests/day
   // const apiKey: string = "05996a44c3aabae82b516a7dfc983208";
-  const apiKey: string = "96605289957c08c1352aaf8bff1793d4";
+  const apiKey: string = "e919766f1ea7f6095f9d487debb02058";
   // took out '&country=us'
   const url: string =
     "https://gnews.io/api/v4/search?q=dinosaur&lang=en&max=10&apikey=" +
