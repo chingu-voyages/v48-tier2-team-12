77f10bb76b18e9bc1df41ae5fce44cfb887cf0d1
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-04-06 18:16:54 +0200

    refactoring news api

--- src/components/NewsCardGrid.tsx
+++ src/components/NewsCardGrid.tsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from 'react';
 import { Article } from '../interfaces/article.interface';
-import { fetchNews } from '../utils/news-api';
+import { fetchNews, isTimeToFetchNews, pickOnlyFewNews, now } from '../utils/news-api';
 import { NewsCard, NewsErrorHandling } from './NewsCard';
-import classes from '../css-modules/NewsCardGrid.module.css';
+import styles from '../css-modules/NewsCardGrid.module.css';
 
 export function NewsCardGrid() {
   const [articles, setArticles] = useState<Article[]>([]);
-  const now = new Date().getTime();
 
   useEffect(() => {
-    // Define an async function inside the effect for fetching news
+    // Define an async function inside the effect for fetching and storing news
     const fetchAndStoreNews = async () => {
-      if (timeComparison()) { // If it's time to fetch
-        const response = await fetchNews(); // Assuming fetchNews is an async function
+      if (isTimeToFetchNews()) { // If it's time to fetch
+        const response = await fetchNews();
         const news = response.articles;
         localStorage.setItem("dinopediaNews", JSON.stringify(news));
         localStorage.setItem('dinopedia-news-timestamp', now.toString());
@@ -30,32 +29,10 @@ export function NewsCardGrid() {
     }
   }, []);
 
-  const timeComparison = () => {
-    const timestamp = localStorage.getItem('dinopedia-news-timestamp');
-    const nextTimeToFetchNews = timestamp ? (Number(timestamp) + 1 * 24 * 60 * 60 * 1000) : now;
-    console.log('nextTimeToFetchNews', nextTimeToFetchNews)
-
-    return !timestamp || now >= nextTimeToFetchNews
-  };
-
-  const pickOnlyFewNews = (articlesArray: Article[]) => {
-    const randomFewNews = [];
-    //picking only 4 out of 10 news randomly now (i < 4)
-    for (let i = 0; i < 4; i++) {
-      randomFewNews.push(
-        articlesArray[Math.floor(Math.random() * articlesArray.length)]
-      );
-    }
-    return randomFewNews
-  }
-
-  const test = () => timeComparison() ? ' Fetching...' : '  More news tomorrow'
-  console.log("Should show 'Fetching...' only on the first time rendering or after 24h of last time the browser fetched news", test())
-
   return (
-    <div className={classes.newsContainer}>
-      <div className={classes.newsCardGridLabel}>News</div>
-      <div className={classes.newsCardGrid}>
+    <div className={styles.newsContainer}>
+      <div className={styles.newsCardGridLabel}>News</div>
+      <div className={styles.newsCardGrid}>
         <div>
           {articles ? (
             pickOnlyFewNews(articles).map((article, index) => (
