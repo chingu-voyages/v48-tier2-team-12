6737223145eef4e4e3492606ca446f376646f7ab
cris-valente <88455718+cris-valente@users.noreply.github.com>
2024-04-06 13:46:21 +0200

    discoverPage and more facts

--- src/components/DinoFacts.tsx
+++ src/components/DinoFacts.tsx
@@ -5,11 +5,15 @@ import styles from '../css-modules/DinoFacts.module.css';
 const dinoFacts = [
   'Did you know that the word “dinosaur” means “terrible lizard” in Greek?',
   'Did you know that some dinosaurs, like the Velociraptor, were covered in feathers?',
-  'Did you know that the smallest known dinosaur is the Microraptor?',
   'Did you know that the dinosaurs ruled Earth for over 160 million years?',
-  'Did you know that birds are descended from dinosaurs?',
+  'Did you know that modern birds are direct descendants of dinosaurs?',
   'Did you know that Triceratops had three horns and a frill?',
   'Did you know that Stegosaurus had plates on it’s back?',
+  'Did you know that the Argentinosaurus, the largest dinosaur, weighed as much as 10 elephants?',
+  'Did you know that some dinosaurs like the Microraptor were as small as chickens?',
+  'Did you know that dinosaur fossils have been found on every continent, including Antarctica?',
+  'Did you know that the fastest dinosaur, the Ornithomimus, could sprint at speeds of up to 64 km/h?',
+  'Did you know that some dinosaurs, like the Stegosaurus, had brains the size of walnuts?',
 ];
 
 const DinoFacts = () => {
@@ -19,9 +23,9 @@ const DinoFacts = () => {
     <div className={styles.dinoFactsContainer}>
       <img src={tRexSkull} />
       {pathname === '/' ? (
-        <h2>Which dino do you want to learn about?</h2>
+        <h2 className={styles.title}>Which dino do you want to learn about?</h2>
       ) : (
-        <h2>{dinoFacts[Math.floor(Math.random() * dinoFacts.length)]}</h2>
+        <h2 className={styles.fact}>{dinoFacts[Math.floor(Math.random() * dinoFacts.length)]}</h2>
       )}
     </div>
   );
