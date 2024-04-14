940f92e23650b080d93b04a7fd945198366bcf8b
Morais-C <morais.carlos@hotmail.com>
2024-04-14 08:47:43 +0100

    SortModal Component

--- src/components/DinoCardGrid.tsx
+++ src/components/DinoCardGrid.tsx
@@ -13,7 +13,6 @@ export function DinoCardGrid({
   dinos: Dino[];
   title: string;
 }) {
-
   const { isOpen, toggle } = useModal(); // using a custom Hook - on /utils
   const [currentSortCriteria, setCurrentSortCriteria] = useState<string>("5");
 
@@ -21,10 +20,50 @@ export function DinoCardGrid({
     setCurrentSortCriteria(n);
   };
 
+  const sortFunction = (d1: Dino, d2: Dino) => {
+    switch (currentSortCriteria) {
+      case "1": {
+        // Weight: Low to High
+        if (d1.weight && d2.weight && d1.weight > d2.weight) return 1;
+        if (d1.weight && d2.weight && d1.weight < d2.weight) return -1;
+        return 0;
+        break;
+      }
 
-  // const sortDinos =  (n: string) => {
-
-  // } 
+      case "2": {
+        // Weight: High to Low
+        if (d1.weight && d2.weight && d1.weight > d2.weight) return -1;
+        if (d1.weight && d2.weight && d1.weight < d2.weight) return 1;
+        break;
+      }
+      case "3": {
+        // Length: Low to Hight
+        if (d1.length && d2.length && d1.length > d2.length) return 1;
+        if (d1.length && d2.length && d1.length < d2.length) return -1;
+        break;
+      }
+      case "4": {
+        // Length: High to Low
+        if (d1.length && d2.length && d1.length > d2.length) return -1;
+        if (d1.length && d2.length && d1.length < d2.length) return 1;
+        break;
+      }
+      case "6": {
+        // Name: Z-A
+        if (d1.name && d2.name && d1.name > d2.name) return -1;
+        if (d1.name && d2.name && d1.name < d2.name) return 1;
+        return 0;
+        break;
+      }
+      default: {
+        // by default order by Name Ascending ( Order Criteria = 5 --> Name: A-Z)
+        if (d1.name && d2.name && d1.name > d2.name) return 1;
+        if (d1.name && d2.name && d1.name < d2.name) return -1;
+        return 0;
+        break;
+      }
+    }
+  };
 
   return (
     <>
@@ -39,8 +78,7 @@ export function DinoCardGrid({
       />
 
       <div className={classes.smallCardGrid}>
-        {dinos.sort()
-        .map((dino, index) => (
+        {dinos.sort(sortFunction).map((dino, index) => (
           <DinoCard
             key={index}
             id={dino.id}
